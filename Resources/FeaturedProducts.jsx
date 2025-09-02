884c3eaa641d632019e3c27bc7ad0bb9f4ca5c07
Mr Blvck <complexjrilo.com>
2025-09-02 10:37:49 +0100

    Price fix

--- Resources/FeaturedProducts.jsx
+++ Resources/FeaturedProducts.jsx
@@ -3,41 +3,41 @@ export const featuredproducts = [
         id: 1,
         image: "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/05/097177/1.jpg",
         description: "Ladies Trend Non Tarnish Exotic/Super Lasting Luxury Ladies Classic Set",
-        price: 68.00,
+        price: 6800,
     },
     {
         id: 2,
         image: "https://www-konga-com-res.cloudinary.com/w_500,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/W/B/239495_1740779890.jpg",
         description: "Full Lace Braided Wigs With Frontal - 16",
-        price: 45.000,
+        price: 45000,
     },
     {
         id: 3,
         image: "https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/05/184717/1.jpg",
         description: "Armaf Club De Nuit Woman EDP 105ml Long Lasting Perfume",
-        price:  58.999,
+        price:  58999,
     }, {
         id: 4,
         image: "https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/51/8001714/1.jpg",
         description: "Lattafa Khamrah EDP 100ml",
-        price: 44.900,
+        price: 44900,
     },{
         id: 5,
         image: "https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/66/0712972/1.jpg",
         description: "FNGEEN Fngeen Top Luxury Mechanical Watch",
-        price:  10.500,
+        price:  10500,
     },
     {
         id: 6,
         image: "https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/37/3441153/1.jpg",
         description: "Women's Watch + Necklace+ Earring+ Ring + Bracelet - Gold",
-        price:  5.747,
+        price:  5747,
     },
     {
         id: 7,
         image: "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/30/7518504/1.jpg",
         description: "Stained Glass Cat Women's Braided Rope Bracelet",
-        price: 2.800,
+        price: 2800,
     },
     {
         id: 8,
@@ -63,7 +63,7 @@ export const featuredproducts = [
         id: 12,
         image: "https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/42/2400431/1.jpg",
         description: "Beautiful-Curly-Weavon-For-Ladies-(4-6bundles) Full Head Fix",
-        price: 12.999,
+        price: 12999,
     }
     
 ]
